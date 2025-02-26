870f90a24f531fda12b561e8e8abb0309614048b
Sanskar Kishan <151910239+sanskarkishan@users.noreply.github.com>
2025-02-26 20:37:21 +0530

    Add files via upload

---
+++ src/main/java/com/springProject/employee_poject/EmployeeEntity.java
@@ -0,0 +1,23 @@
+package com.springProject.employee_poject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String phone;
+    private String email;
+    private String depart;
+    
+}
